2418c9f7fe62ab2502267165cbfbc07a81dbe176
Md Saiful Saif <mdsaifulislam02137@gmail.com>
2025-11-24 19:05:58 +0600

    array flat

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ const featuredAccesssRole = ["admin", "manager"];
 
 // array from
 // const arr = Array.from({ length: 5 }).fill("0");
-const arr = Array.from([1, 2, 3], (val) => val * 2);
+// const arr = Array.from([1, 2, 3], (val) => val * 2);
 
-console.log(arr);
+const arr = [22, 33, 55, [1, 2, 3], [4, 3, 4, 5], [100, 200]];
+
+const result = arr.flat(Infinity);
+
+console.log(result);
