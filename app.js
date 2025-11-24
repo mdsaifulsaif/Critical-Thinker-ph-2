64d8c1b91acb73886778db97387cca81e557dddc
Md Saiful Saif <mdsaifulislam02137@gmail.com>
2025-11-24 18:55:26 +0600

    array from

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const currentuser = ["user", "editor"];
 
 const featuredAccesssRole = ["admin", "manager"];
 
-const user = currentuser.some((role) => console.log(role));
+// const user = currentuser.some((role) => console.log(role));
 
-console.log(user);
+// array from
+// const arr = Array.from({ length: 5 }).fill("0");
+const arr = Array.from([1, 2, 3], (val) => val * 2);
+
+console.log(arr);
